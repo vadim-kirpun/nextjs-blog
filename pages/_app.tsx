0298b126c38435cf07ac403ee233503bb700831b
Vadim Kirpun <quinze105@gmail.com>
2021-10-23 13:25:47 +0300

    init project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle, theme } from 'styles';
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>
+);
+
+export default MyApp;
