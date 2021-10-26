4bb14eac3cafbb2af7041fcff218ba64b5545c24
Vadim Kirpun <quinze105@gmail.com>
2021-10-26 23:55:02 +0300

    minimize build size with light version of syntax highlighter

--- modules/post-details/PostBody.tsx
+++ modules/post-details/PostBody.tsx
@@ -1,12 +1,17 @@
 import { memo } from 'react';
 import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
+import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css';
 
 import type { IPost } from '@types';
 
 import { ImageWrapper } from './styles/PostBody';
+
+SyntaxHighlighter.registerLanguage('js', js);
+SyntaxHighlighter.registerLanguage('css', css);
 
 type Props = Pick<IPost, 'slug' | 'content'>;
 
